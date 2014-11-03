3e4222c50ae31d82d5f7f3cbd536e84fc0d6d483
my-zhang <zhangmengyu.grad@gmail.com>
2014-11-02 21:56:12 -0600

    try File IO

--- ch07-vm/basic/src/main/scala/BasicTranslator.scala
+++ ch07-vm/basic/src/main/scala/BasicTranslator.scala
@@ -1,5 +1,32 @@
+import scala.io.Source
+
 object BasicTranslator {
-  def main(args: Array[String]) { 
-    println("Hi!")
+
+  def rmComment(s: String): String = { 
+    val commentPattern = """\s*//.*""".r 
+    return commentPattern.replaceAllIn(s, "") 
+  }
+
+  def isEmptyLine(s: String): Boolean = { 
+    return s.trim.length == 0 
+  }
+
+  def trimInput(lines: Array[String]): Array[String] = { 
+    return lines.map(s => rmComment(s)).filter(s => !isEmptyLine(s)) 
+  }
+
+  def main(args: Array[String]): Unit = { 
+    if (args.length == 0) { 
+      println("specify a input file or dir.")
+      return 
+    }
+
+    val fd = Source.fromFile(args(0))
+    val lines = fd.getLines.toArray
+    val exprs = trimInput(lines) 
+
+    for (e <- exprs) { 
+      println(e) 
+    }
   }
 }
